c1e0782e8c9c020852c79db18d46c593e2138792
Vinrobot <vinrobot97@gmail.com>
2023-06-13 01:46:39 +0200

    New project from template generator

--- src/client/java/net/vinrobot/seventv/client/mixin/SevenTVClientMixin.java
+++ src/client/java/net/vinrobot/seventv/client/mixin/SevenTVClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin.client;
+package net.vinrobot.seventv.client.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
+public class SevenTVClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void run(CallbackInfo info) {
 		// This code is injected into the start of MinecraftClient.run()V
--- src/main/java/net/vinrobot/seventv/mixin/SevenTVMixin.java
+++ src/main/java/net/vinrobot/seventv/mixin/SevenTVMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package net.vinrobot.seventv.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
+public class SevenTVMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
