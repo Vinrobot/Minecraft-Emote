69517552e068c7631ea04cd0304d23031516c7f9
Vinrobot <vinrobot97@gmail.com>
2023-06-14 23:26:27 +0200

    Remove MixinExtras

--- src/client/java/net/vinrobot/seventv/client/SevenTVModClient.java
+++ src/client/java/net/vinrobot/seventv/client/SevenTVModClient.java
@@ -1,6 +1,5 @@
 package net.vinrobot.seventv.client;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import net.fabricmc.api.ClientModInitializer;
 import net.vinrobot.seventv.client.text.EmotesManager;
 
@@ -10,6 +9,5 @@ public class SevenTVModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		MixinExtrasBootstrap.init();
 	}
 }
--- src/client/java/net/vinrobot/seventv/client/mixin/ChatHudMixin.java
+++ src/client/java/net/vinrobot/seventv/client/mixin/ChatHudMixin.java
@@ -1,7 +1,5 @@
 package net.vinrobot.seventv.client.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.font.TextRenderer;
@@ -11,13 +9,14 @@ import net.minecraft.text.OrderedText;
 import net.vinrobot.seventv.client.text.EmoteParser;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ChatHud.class)
 @Environment(EnvType.CLIENT)
 public class ChatHudMixin {
-	@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/OrderedText;III)I"))
-	private int drawTextWithShadow(DrawContext drawContext, TextRenderer textRenderer, OrderedText orderedText, int x, int y, int color, Operation<Integer> original) {
+	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/OrderedText;III)I"))
+	private int drawTextWithShadow(DrawContext instance, TextRenderer textRenderer, OrderedText orderedText, int x, int y, int color) {
 		orderedText = EmoteParser.wrapOrderedText(orderedText);
-		return original.call(drawContext, textRenderer, orderedText, x, y, color);
+		return instance.drawTextWithShadow(textRenderer, orderedText, x, y, color);
 	}
 }
